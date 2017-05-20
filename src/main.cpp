26d020454328412ff87a09ea51b9e5224e8e1429
andrekorol <anrobits@yahoo.com.br>
2017-05-20 03:28:33 -0300

    Add Bluetooth library

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
+int relay1 = 2;
+
+byte serialA;
 void setup() {
+    Serial.begin(9600);
+    pinMode(relay1, OUTPUT);
 
 }
 
 void loop() {
-    
+    if (Serial.available() > 0) {
+	serialA = Serial.read();Serial.println(serialA);
+    }
+    switch (serialA) {
+    case 1:
+	digitalWrite(relay1, HIGH);
+	break;
+    case 2:
+	digitalWrite(relay1, LOW);
+	break;
+    case 3:
+	delay(100);
+	digitalWrite(relay1, HIGH);
+	delay(100);
+	break;
+    case 4:
+	digitalWrite(relay1, LOW);
+    default:
+	break;
+    }
 }
