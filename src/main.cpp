e3e9df3685e83edc63f759bd3b1dab8acfd6f5c6
andrekorol <anrobits@yahoo.com.br>
2017-05-20 13:30:25 -0300

    Add Clion project

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 
-int relay1 = 2;
+uint8_t relay1 = 2;
 
 byte serialA;
 void setup() {
@@ -12,7 +11,7 @@ void setup() {
 
 void loop() {
     if (Serial.available() > 0) {
-	serialA = Serial.read();Serial.println(serialA);
+	serialA = (byte) Serial.read();Serial.println(serialA);
     }
     switch (serialA) {
     case 1:
